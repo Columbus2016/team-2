b1d2154bc0e9ac7876612cadbd420a5deb50b31f
Ivy Lei <ivylei009@gmail.com>
2016-10-14 22:43:39 -0500

    #makeandroidstudiogreatagain

--- app/src/main/java/com/example/club084/feedamerica/LoginActivity.java
+++ app/src/main/java/com/example/club084/feedamerica/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.club084.feedamerica;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -64,6 +65,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
@@ -87,11 +89,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
+                Intent intent = new Intent(LoginActivity.this, ChallengeActivity.class);
+                startActivity(intent);
             }
         });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+
     }
 
     private void populateAutoComplete() {
